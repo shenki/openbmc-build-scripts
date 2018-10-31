d443074684f5d9b956fa13b71d42ba867ddc6cbc
Joel Stanley <joel@jms.id.au>
2018-10-31 19:27:00 +1030

    qemu-build tests
    Change-Id: I589f5112ede796085c321f30dd6a78710cef25b0
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- qemu-build.sh
+++ qemu-build.sh
@@ -1,50 +1,51 @@
 #!/bin/bash
-
-# This build script is for running the Jenkins builds using docker.
-
+###############################################################################
+#
+# This build script is for running the QEMU build as a container
+#
+###############################################################################
+#
+# Script Variables:
+#  http_proxy         The HTTP address of the proxy server to connect to.
+#                     Default: "", proxy is not setup if this is not set
+#  WORKSPACE          Path of the workspace directory where some intermediate
+#                     files and the images will be saved to.
+#                     Default: "~/{RandomNumber}"
+#
+###############################################################################
 # Trace bash processing
 set -x
 
-# Default variables
-WORKSPACE=${WORKSPACE:-${HOME}/${RANDOM}${RANDOM}}
+# Script Variables:
 http_proxy=${http_proxy:-}
+WORKSPACE=${WORKSPACE:-${HOME}/${RANDOM}${RANDOM}}
+
+# Determine the architecture
+ARCH=$(uname -m)
+
+# Docker Image Build Variables:
+img_name=qemu-build
 
 # Timestamp for job
 echo "Build started, $(date)"
 
-# Configure docker build
+# Setup Proxy
 if [[ -n "${http_proxy}" ]]; then
 PROXY="RUN echo \"Acquire::http::Proxy \\"\"${http_proxy}/\\"\";\" > /etc/apt/apt.conf.d/000apt-cacher-ng-proxy"
 fi
 
-Dockerfile=$(cat << EOF
-FROM ubuntu:16.04
-
-${PROXY}
-
-ENV DEBIAN_FRONTEND noninteractive
-RUN apt-get update && apt-get install -yy --no-install-recommends \
-	flex bison \
-	libpixman-1-dev \
-	libglib2.0-dev \
-	libfdt-dev \
-	make python-yaml gcc libc6-dev
-
-RUN grep -q ${GROUPS} /etc/group || groupadd -g ${GROUPS} ${USER}
-RUN grep -q ${UID} /etc/passwd || useradd -d ${HOME} -m -u ${UID} -g ${GROUPS} ${USER}
-
-USER ${USER}
-ENV HOME ${HOME}
-RUN /bin/bash
-EOF
-)
-
-# Build the docker container
-docker build -t qemu-build/ubuntu - <<< "${Dockerfile}"
-if [[ "$?" -ne 0 ]]; then
-  echo "Failed to build docker container."
-  exit 1
-fi
+# Determine the prefix of the Dockerfile's base image
+case ${ARCH} in
+  "ppc64le")
+    DOCKER_BASE="ppc64le/"
+    ;;
+  "x86_64")
+    DOCKER_BASE=""
+    ;;
+  *)
+    echo "Unsupported system architecture(${ARCH}) found for docker image"
+    exit 1
+esac
 
 # Create the docker run script
 export PROXY_HOST=${http_proxy/#http*:\/\/}
@@ -58,23 +59,72 @@ cat > "${WORKSPACE}"/build.sh << EOF_SCRIPT
 
 set -x
 
-cd ${WORKSPACE}
+# Go into the build directory
+cd ${WORKSPACE}/qemu 
 
 gcc --version
-
-# Go into the source directory (the script will put us in a build subdir)
-cd qemu
+git submodule update --init dtc
 # disable anything that requires us to pull in X
-./configure --target-list=arm-softmmu \
-	--disable-spice --disable-docs --disable-gtk --disable-smartcard \
-	--disable-usb-redir --disable-libusb --disable-sdl --disable-gnutls \
-	--disable-vte --disable-vnc --disable-vnc-png
+./configure \
+    --target-list=arm-softmmu \
+    --disable-spice \
+    --disable-docs \
+    --disable-gtk \
+    --disable-smartcard \
+    --disable-usb-redir \
+    --disable-libusb \
+    --disable-sdl \
+    --disable-gnutls \
+    --disable-vte \
+    --disable-vnc \
+    --disable-werror \
+    --disable-vnc-png
+make clean
 make -j4
 
 EOF_SCRIPT
 
 chmod a+x ${WORKSPACE}/build.sh
 
-# Run the docker container, execute the build script we just built
-docker run --rm=true -e WORKSPACE=${WORKSPACE} --user="${USER}" \
-  -w "${HOME}" -v "${HOME}":"${HOME}" -t qemu-build/ubuntu ${WORKSPACE}/build.sh
+# Configure docker build
+Dockerfile=$(cat << EOF
+FROM ${DOCKER_BASE}ubuntu:16.04
+
+${PROXY}
+
+ENV DEBIAN_FRONTEND noninteractive
+RUN apt-get update && apt-get install -yy --no-install-recommends \
+    bison \
+    flex \
+    gcc \
+    git \
+    libc6-dev \
+    libfdt-dev \
+    libglib2.0-dev \
+    libpixman-1-dev \
+    make \
+    python-yaml \
+    python3-yaml \
+    iputils-ping
+
+RUN grep -q ${GROUPS} /etc/group || groupadd -g ${GROUPS} ${USER}
+RUN grep -q ${UID} /etc/passwd || useradd -d ${HOME} -m -u ${UID} -g ${GROUPS} ${USER}
+USER ${USER}
+ENV HOME ${HOME}
+EOF
+)
+
+docker build -t ${img_name} - <<< "${Dockerfile}"
+if [[ "$?" -ne 0 ]]; then
+  echo "Failed to build docker container."
+  exit 1
+fi
+
+docker run \
+    --rm=true \
+    -e WORKSPACE=${WORKSPACE} \
+    -w "${HOME}" \
+    --user="${USER}" \
+    -v "${HOME}":"${HOME}" \
+    -t ${img_name} \
+    ${WORKSPACE}/build.sh
